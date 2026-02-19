8b23edec1962dec31f04cc633a3506f0ee9a52e8
Zoex-12 <manilpandey27@gmail.com>
2026-02-19 12:03:49 +0545

    add script, style and index file to the code base

---
+++ index.js
@@ -0,0 +1,49 @@
+// JavaScript for TicTac Toe game
+
+const boxes = document.querySelectorAll(".item");
+const head = document.getElementById("head");
+const ticTac = document.querySelector(".playGround");
+
+document.body.style.color = "white";
+
+let winConditions = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+let currentPlayer = "X";
+
+function StartGame(e) {
+  if (e.target.innerText === "") {
+    e.target.innerText = currentPlayer;
+    winner();
+
+    currentPlayer = currentPlayer === "X" ? "O" : "X";
+  }
+}
+ticTac.addEventListener("click", StartGame);
+
+function winner() {
+  winConditions.forEach((item) => {
+    let index0 = item[0];
+    let index1 = item[1];
+    let index2 = item[2];
+
+    let value0 = boxes[index0].innerText;
+    let value1 = boxes[index1].innerText;
+    let value2 = boxes[index2].innerText;
+
+    if (value0 !== "" && value1 !== "" && value2 !== "") {
+      if (value0 === value1 && value1 === value2) {
+        head.innerText = `winner is ${value0}`;
+        ticTac.removeEventListener("click", StartGame);
+      }
+    }
+  });
+}
